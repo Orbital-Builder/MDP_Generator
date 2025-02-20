9278bdd3c49da2dad784ce2d770d63ea541fe4e5
Orbital-Builder <mb35.bn@gmail.com>
2025-02-20 19:45:48 +0100

    1.4
    added the ability to generate 2 different passwords, one without special characters, the other with special characters.

--- MDP_Generator/MDP_Generator.py
+++ MDP_Generator/MDP_Generator.py
@@ -4,28 +4,40 @@ import random
 
 
 Code by Orbital-Builder.
-Aide : Github Copilot
-Aide : UDEMY - Jean-Philippe Parein  
-Version 1.0.
+Aide : Github Copilot.
+Aide : UDEMY - Jean-Philippe Parein. 
+Version 1.4.
 -----------------------------------
 
 
 """
+
 
 def generer_mot_de_passe(longueur):
     caracteres = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
     mot_de_passe = "".join(random.sample(caracteres, longueur))
     return mot_de_passe
 
-def extraire_mdp_fichier(nom_fichier, longueur, nb_mdp):
+def generer_mot_de_passe_caractere(long):
+    carac_spe = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()-_=+[]{}|;:,.<>?/~`"
+    mdp = "".join(random.sample(carac_spe, long))
+    return mdp
+
+def extraire_mdp_fichier(nom_fichier, longueur, nb_mdp, special=False):
     with open(nom_fichier, "w") as fichier:
         for i in range(nb_mdp):
-            mdp = generer_mot_de_passe(longueur)
+            if special:
+                mdp = generer_mot_de_passe_caractere(longueur)
+            else:
+                mdp = generer_mot_de_passe(longueur)
             fichier.write(f"{mdp}\n")
 
 if __name__ == "__main__":
     extraire_mdp_fichier("mot_de_passe.txt", 18, 15)
     print("Les mots de passe ont été générés et enregistrés dans le fichier mot_de_passe.txt")
+    
+    extraire_mdp_fichier("mot_de_passe_caractères.txt", 18, 15, special=True)
+    print("Les mots de passe avec caractères spéciaux ont été générés et enregistrés dans le fichier mot_de_passe_caractères.txt")
 
 
 
